e18f6bb9ac6907ae701646258832b3389d8e4dcc
vaishnavi <landevaishnavi342@gmail.com>
2024-04-26 22:17:45 +0530

    models change commit

--- backend/JusticeFY_backend/JusticeFY_app/admin.py
+++ backend/JusticeFY_backend/JusticeFY_app/admin.py
@@ -1,3 +1,29 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin.sites import site 
+from .models import Lawyer
+from .models import PhysicalJudge
+from .models import VirtualJudge
+from django.contrib import admin
+from JusticeFY_app.models import Lawyer 
+from JusticeFY_app.models import PhysicalJudge;
+from JusticeFY_app.models import VirtualJudge;
+
+
+@admin.register(Lawyer)
+class LawyerAdmin(admin.ModelAdmin):
+    list_display = ('CNR', 'ClientName', 'CaseStatus', 'CaseType', 'Doc', 'ApplytoVC')
+admin.site.register(Lawyer,LawyerAdmin)
+
+
+@admin.register(PhysicalJudge)
+class PhysicalJudgeAdmin(admin.ModelAdmin):
+    list_display = ('CNR', 'CaseStatus', 'Lawyer_1', 'Lawyer_2', 'Lawyer1ASVC', 'Lawyer2ASVC', 'Approve')
+admin.site.register(PhysicalJudge,PhysicalJudgeAdmin)
+
+
+@admin.register(VirtualJudge)
+class VirtualJudgeAdmin(admin.ModelAdmin):
+    list_display = ('CNR', 'CaseStatus', 'Lawyer_1', 'Lawyer_2', 'virtualcourt')
+admin.site.register(VirtualJudge,VirtualJudgeAdmin)
+
